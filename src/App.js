6905c7bd3e3ab80d0badcf86f8452df39a8d9720
markkmwaura <markkmwaura@gmail.com>
2020-06-28 02:28:45 +0300

    using async actions with thunk middleware

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import IceCreamContainer from './components/IceCreamContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div className='App'>
-				<ItemContainer cake />
+				{/* <ItemContainer cake />
 				<ItemContainer />
 				<HooksCakeContainer />
 				<CakeContainer />
 				<IceCreamContainer />
-				<NewCakeContainer />
+				<NewCakeContainer /> */}
+				<UserContainer/>
 			</div>
 		</Provider>
 	);
